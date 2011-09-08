a3637815be647a3b284c2264d455c594a33286b4
Justin Beck <justinbeck@mac.com>
2011-09-08 10:21:52 -0400

    Specs are now working.  Not sure if this is the right approach.

--- spec/quiz/node_spec.rb
+++ spec/quiz/node_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 module Quiz
   describe Node do
     let(:output) { double('output').as_null_object }
-    let(:node) { Node.new(output) }
 
     it "should ask if I'm Justin" do
+      node = Node.new(output)
       output.should_receive(:puts).with("Are you Justin?")
       node.ask
     end
@@ -13,17 +13,19 @@ module Quiz
     context "answering" do
       context "when a user answers 'no'" do
         it "should say 'You win!'" do
+          node = Node.new(output)
           node.stub!(:gets) { "no\n" }
-          node.ask
           output.should_receive(:puts).with("You win!")
+          node.ask
         end
       end
 
       context "when a user answers 'yes'" do
         it "should say 'I win!'" do
-          node.ask
-          puts "yes\n"
+          node = Node.new(output)
+          node.stub!(:gets) { "yes\n" }
           output.should_receive(:puts).with("I win!")
+          node.ask
         end
       end
     end
