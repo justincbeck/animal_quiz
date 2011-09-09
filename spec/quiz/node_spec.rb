1e5a3e7684953c2fbae21bd4e8b6300b522b9532
Justin Beck <justinbeck@mac.com>
2011-09-08 22:02:46 -0400

    Resolved broken tests by fixing stub of gets

--- spec/quiz/node_spec.rb
+++ spec/quiz/node_spec.rb
@@ -4,35 +4,35 @@ module Quiz
   describe Node do
     let(:output) { double('output').as_null_object }
 
+    before(:each) do
+      @node = Node.new(output)
+      @node.question = "Are you a bear?"
+      @node.animal = "bear"
+      @node.root = @node
+      @node.parent = @node
+    end
+
     context "asking" do
       it "should ask if I'm a bear" do
-        node = Node.new(output)
-        node.question = "Are you a bear?"
         output.should_receive(:puts).with("Are you a bear?")
-        node.ask
+        @node.ask
       end
     end
 
     context "answering" do
       context "when a user answers 'no'" do
         it "should say 'You win!'" do
-          node = Node.new(output)
-          node.animal = "bear"
-          node.question = "Are you a bear?"
-          node.stub!(:gets) { "no\n" }
           output.should_receive(:puts).with("You win! Help me learn from my mistake before you go...")
-          node.ask
+          @node.stub!(:gets).and_return('no')
+          @node.ask
         end
       end
 
       context "when a user answers 'yes'" do
         it "should say 'I win!'" do
-          node = Node.new(output)
-          node.question = "Are you a bear?"
-          node.stub!(:gets) { "yes\n" }
-          node.animal = "bear"
           output.should_receive(:puts).with("I win!")
-          node.ask
+          @node.stub!(:gets).and_return('yes', 'no')
+          @node.ask
         end
       end
     end
